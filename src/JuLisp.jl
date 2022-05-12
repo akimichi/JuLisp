22a42f611d5f0aaa0ca94d857565ecd2669a07c5
Akimichi Tatsukawa <akimichi.tatsukawa@gmail.com>
2022-05-12 15:51:52 +0900

    numberの定義を訂正した

--- src/JuLisp.jl
+++ src/JuLisp.jl
@@ -25,7 +25,11 @@ end
 struct Num <: Object
     value::Number
 end
-number(value::String) = Num(value)
+function number(value::String)
+  num = parse(Float64, value)
+  isinteger(num) ? Int(num) : num
+  Num(num)
+end
 show(io::IO, n::Num) = print(io, n.value)
 
 
