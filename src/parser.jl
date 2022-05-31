66b044fc509e2df7f32cf22a219bfea41a0dec0c
Akimichi Tatsukawa <akimichi.tatsukawa@gmail.com>
2022-05-31 18:12:22 +0900

    Num, Str のパースに成功

--- src/parser.jl
+++ src/parser.jl
@@ -1,7 +1,12 @@
 using ParserCombinator
 
-num = PInt64() > Num
-exp = num + Eos()
+integer_number = PInt64() > Num
+float_number = PFloat64() > Num
+num = integer_number | float_number
+word = Word()
+string_token = E"\"" + word + E"\"" > Str
+atom_token = num | string_token
+exp = atom_token + Eos()
 
 
 function parser(s::String)
--- test/parser.jl
+++ test/parser.jl
@@ -1,5 +1,10 @@
 @testset "parser" begin
-
+  @testset "number" begin
     @test Num(1) == parser("1")
-
+    @test Num(0.01) == parser("0.01")
+    @test Num(-30) == parser("-30")
+  end
+  @testset "string" begin
+    @test Str("abc") == parser("\"abc\"")
+  end
 end
